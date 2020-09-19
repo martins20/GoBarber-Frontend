05b8d3f29b9bdfeeceb3e938974636600206d3c8
martins20 <pdesignerpb@gmail.com>
2020-09-19 13:09:44 -0300

    Finish SignIn style Page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
 
-// import { Container } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
-  return <h1>SignIn</h1>;
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input placeholder="E-mail" />
+
+          <input type="password" placeholder="Senha" />
+
+          <button>Entrar</button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </form>
+        <a href="">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
 };
 
 export default SignIn;
