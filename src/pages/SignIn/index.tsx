4eb61c5127643ba0075cf39aadecf71da1acaefa
martins20 <pdesignerpb@gmail.com>
2020-09-20 23:05:48 -0300

    Auto destroy toast after 3 seconds

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -55,7 +55,11 @@ const SignIn: React.FC = () => {
         }
 
         // create a toast
-        addToast();
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+        });
       }
     },
     [signIn, addToast],
